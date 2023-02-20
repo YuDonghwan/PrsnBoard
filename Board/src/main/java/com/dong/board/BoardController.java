bb869dda7aa82c04a9610006d0d816e6f8427a9b
유동환 <ydhwan0905@gmail.com>
2023-02-20 09:16:17 +0900

    깃 테스트

---
+++ Board/src/main/java/com/dong/board/BoardController.java
@@ -0,0 +1,16 @@
+package com.dong.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+	@GetMapping("/board")
+	public String board() {
+		
+		
+		
+		return "board";
+	}
+}
